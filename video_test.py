fe8093d4aa490fcea8337f26399c749f6014df9b
Jaden Wang <jadenyjw@gmail.com>
2017-05-15 11:24:05 -0400

    Get key holding to work

--- video_test.py
+++ video_test.py
@@ -7,9 +7,24 @@ import cv2
 import sys
 import os
 
+
 class Application:
+
+
+
     def __init__(self):
 
+        def leftKey(event):
+            self.leftKeyDown = True
+        def rightKey(event):
+            self.rightKeyDown = True
+        def leftKeyReleased(event):
+            self.leftKeyDown = False
+        def rightKeyReleased(event):
+            self.rightKeyDown = False
+
+        self.leftKeyDown = False
+        self.rightKeyDown = False
         self.vs = cv2.VideoCapture("http://192.168.12.70:8080/video") # capture video frames, 0 is your default video
         print(self.vs)
         self.current_image = None  # current image from the camera
@@ -20,29 +35,37 @@ class Application:
         self.panel = tk.Label(self.root)  # initialize image panel
         self.panel.pack(padx=10, pady=10)
         self.root.config(cursor="arrow")
+        self.root.bind('<Left>', leftKey)
+        self.root.bind('<Right>', rightKey)
+        self.root.bind("<KeyRelease-Left>", leftKeyReleased)
+        self.root.bind("<KeyRelease-Right>", rightKeyReleased)
 
 
         # start a self.video_loop that constantly pools the video sensor
         # for the most recently read frame
         self.video_loop()
+        self.key_loop()
 
+    def key_loop(self):
+        if(self.rightKeyDown):
+            print("Right key pressed")
+        if(self.leftKeyDown):
+            print("Left key pressed")
+        print("filler")
+        self.root.after(100, self.key_loop)
 
     def video_loop(self):
         """ Get frame from the video stream and show it in Tkinter """
         ok, frame = self.vs.read()  # read frame from video stream
 
-#        frame = cv2.resize(frame, (1500,1000))
         if ok:  # frame captured without any errors
-            #key = cv2.waitKey()
             cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)  # convert colors from BGR to RGBA
             self.current_image = Image.fromarray(cv2image)  # convert image for PIL
-            resized_image = self.current_image.resize([800, 600],PIL.Image.ANTIALIAS)
+            resized_image = self.current_image.resize([880, 720],PIL.Image.ANTIALIAS)
             imgtk = ImageTk.PhotoImage(image=resized_image)  # convert image for tkinter
             self.panel.imgtk = imgtk  # anchor imgtk so it does not be deleted by garbage-collector
             self.panel.config(image=imgtk)  # show the image
-            #self.root.attributes("-fullscreen",True)
         self.root.after(1, self.video_loop)  # call the same function after 30 milliseconds
-
 
     def destructor(self):
         """ Destroy the root object and release all resources """
